50927be8d167835f1e4ec637bd2fc7d42c087eed
evil7 <evil7@users.noreply.github.com>
2017-10-12 18:06:04 +0800

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@
  * By evil7@deePwn
  */
 
-var https = require('https'),
+var http = require('http'),
 	WebSocket = require("ws"),
 	net = require('net'),
 	fs = require('fs');
@@ -19,7 +19,7 @@ conf.lport = process.env.PORT || conf.lport;
 conf.domain = process.env.DOMAIN || conf.domain;
 
 //HTTP srv
-var web = https.createServer((req, res) => {
+var web = http.createServer((req, res) => {
 	req.url = (req.url === '/') ? '/index.html' : req.url;
 	fs.readFile(__dirname + '/web' + req.url, (err, buf) => {
 		if (err) {
